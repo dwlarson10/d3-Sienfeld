dbd4973f0507f120fafbfe5ee47a737646d3a805
Daniel Larson <dwlarson10@gmail.com>
2018-01-25 15:31:08 -0500

    Update scatter.js

--- scatter.js
+++ scatter.js
@@ -140,11 +140,28 @@ svg.append('g')
   .style('text-anchor','end')
   .text('trust')
 
-svg.append("g")
-    .attr("class","legend")
-    .attr("transform","translate(50,30)")
-    .style("font-size","12px")
-    .call(d3.legend)
+  // draw legend
+  var legend = svg.selectAll(".legend")
+      .data(color.domain())
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  // draw legend colored rectangles
+  legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+  // draw legend text
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d;})
+});
 
 function yChange() {
 var value = this.value // get the new y value
